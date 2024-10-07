d05cc72524f18c76eae5d19db78def89f6f0d962
abhimvp <abhishek04698@gmail.com>
2024-10-07 00:02:32 -0500

    Move State management to Context

--- ztm-notion-clone/ztm-notion-clone/src/App.tsx
+++ ztm-notion-clone/ztm-notion-clone/src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { AppStateProvider } from "./state/AppStateContext";
 // import { Cover } from "./Page/Cover";
+
 import { Page } from "./Page/Page";
+import createPage from "./utils/createPage";
+
+const initialState = createPage();
+
 function App() {
   return (
-    <>
+    <AppStateProvider initialState={initialState}>
       <Page />
-    </>
+    </AppStateProvider>
   );
 }
 
